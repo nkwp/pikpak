b575b28eb9184bd593be7b5906f4e1f35b6fd6e8
nkwp <95098666+nkwp@users.noreply.github.com>
2021-11-28 00:54:37 +0800

    Update index.vue

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -12,7 +12,7 @@
       bordered
     >
       <a href="#" target="_blank" class="logo-box">
-        <img src="/logo.png" class="logo-box__icon" alt="">
+        <img src="https://mypikpak.com/logo.png" class="logo-box__icon" alt="">
         <div class="logo-box__text">PikPak</div>
       </a>
       <n-menu :options="menuOptions" :value="String(route.name)" @update:value="goRoute"></n-menu>
